28c253e87baa9b5f06dee4f734cfc6b2768f950c
J. Vivian Belenky <j.vivian.belenky@outlook.com>
2026-02-10 17:13:52 -0500

    Fix view snap camera orbit direction by swapping left/right X positions
    Only swap left/right (and iso corners) to correct the winding order,
    keeping front/back unchanged. Clicking clockwise around the view snap
    grid now produces a clockwise camera orbit as expected.

--- ui/src/lib/components/Scene.svelte
+++ ui/src/lib/components/Scene.svelte
@@ -152,17 +152,17 @@
 			case 'back':
 				return [roomCenter.x, roomCenter.y, roomDims.y + dist];
 			case 'left':
-				return [-dist, roomCenter.y, roomCenter.z];
-			case 'right':
 				return [roomDims.x + dist, roomCenter.y, roomCenter.z];
+			case 'right':
+				return [-dist, roomCenter.y, roomCenter.z];
 			case 'iso-front-left':
-				return [-isoDist, isoHeight, -isoDist];
-			case 'iso-front-right':
 				return [roomDims.x + isoDist, isoHeight, -isoDist];
+			case 'iso-front-right':
+				return [-isoDist, isoHeight, -isoDist];
 			case 'iso-back-left':
-				return [-isoDist, isoHeight, roomDims.y + isoDist];
-			case 'iso-back-right':
 				return [roomDims.x + isoDist, isoHeight, roomDims.y + isoDist];
+			case 'iso-back-right':
+				return [-isoDist, isoHeight, roomDims.y + isoDist];
 			default:
 				return null;
 		}
